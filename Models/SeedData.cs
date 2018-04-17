6ceced3745087acd1a5b784160e398c1493a792f
Tim Fleming <tfleming26@hotmail.com>
2018-04-17 10:08:37 -0600

    Seed database

---
+++ Models/SeedData.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace TaskList.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new TaskContext(
+                serviceProvider.GetRequiredService<DbContextOptions<TaskContext>>()))
+                {
+                   if (context.Task.Any())
+                   {
+                       return;
+                   }
+
+                   context.Task.AddRange(
+                       new Task
+                       {
+                           TaskDescription = "Complete Taxes",
+                           DueDate = DateTime.Parse("2018-04-30")
+                       },
+
+                       new Task
+                       {
+                           TaskDescription = "Pay the Power Bill",
+                           DueDate = DateTime.Parse("2018-04-13")
+                       },
+
+                       new Task
+                       {
+                           TaskDescription = "Wash the Jeep",
+                           DueDate = DateTime.Parse("2018-04-28")
+                       },
+
+                       new Task
+                       {
+                           TaskDescription = "Take the Trash Out",
+                           DueDate = DateTime.Parse("2018-05-25")
+                       }
+                   );
+                   context.SaveChanges();
+                }
+            }
+        }
+    }
