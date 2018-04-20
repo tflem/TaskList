0f4e24d0d090185b36b6a6383bac5dc965fceb25
Tim Fleming <tfleming26@hotmail.com>
2018-04-20 10:50:12 -0600

    Add validation rules to task model

--- Models/Task.cs
+++ Models/Task.cs
@@ -6,11 +6,18 @@ namespace TaskList.Models
     public class Task
     {
         public int ID { get; set; }
+        
+        [StringLength(60, MinimumLength = 8)]
+        [Required]
         public string TaskDescription { get; set; }
 
         [Display(Name = "Due Date")]
         [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }    
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(15)]
+        [Required]
         public string Priority { get; set; }    
     }
 }
